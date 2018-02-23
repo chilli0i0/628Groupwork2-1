7448d1c85fe71e583ba268d318bbe648febd416f
Lixia Yi <yilixia@yilixiadeMacBook-Pro.local>
2018-02-22 19:46:12 -0600

    New Folder

---
+++ Code/Lixia Yi/lixia_yi_manipulate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 14:16:44 2018
+
+@author: yilixia
+"""
+
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+import time
+
+stop = set(stopwords.)
